cdb4c1e019242583dec1b81ce4cf02725838eeb6
VuslatYolcu <ylcvalt@gmail.com>
2023-03-29 20:29:39 +0300

    Create models

--- BuilderPattern.playground/Contents.swift
+++ BuilderPattern.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Hamburger {
+    
+    let meat: Meat
+    let sauce: Sauces
+    let toppings: Toppings
+}
+
+enum Meat: String {
+    case beef
+    case chicken
+    case tofu
+}
+
+// Optionset allows for multiple choices
+struct Sauces: OptionSet {
+    let rawValue: Int
+    
+    init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    static let mayonnaise = Sauces(rawValue: 1 << 0)
+    static let mustard = Sauces(rawValue: 1 << 1)
+    static let ketchup = Sauces(rawValue: 1 << 2)
+}
+
+struct Toppings: OptionSet {
+    let rawValue: Int
+    
+    init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    static let cheese = Sauces(rawValue: 1 << 0)
+    static let lettuce = Sauces(rawValue: 1 << 1)
+    static let pickles = Sauces(rawValue: 1 << 2)
+}
